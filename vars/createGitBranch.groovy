c4229d5454cecbfe5facccbdd94843a29c66f206
Jon Lorusso <jon@swatt.exchange>
2018-08-20 19:18:13 +0000

    Use withCredentials instead of credentials (?)

--- vars/createGitBranch.groovy
+++ vars/createGitBranch.groovy
@@ -2,17 +2,14 @@ def call(params) {
   scmUrl = params.scmUrl ? params.scmUrl : scm.getUserRemoteConfigs()[0].getUrl().replaceAll('https://', '') 
   gitCredentialsId = params.gitCredentialsId ? params.gitCredentialsId : '80610dce-f3b7-428e-b69f-956eb087225d'
   
-  sh("echo ${scmUrl}")
-  sh("echo ${gitCredentialsId}")
-  GIT_CREDENTIALS = credentials('80610dce-f3b7-428e-b69f-956eb087225d')
-  sh("echo got credentials")
-  GIT_USERNAME = "${env.GIT_CREDENTIALS_USR}"
-  sh("echo username: ${GIT_USERNAME}")
-  GIT_PASSWORD = java.net.URLEncoder.encode("${env.GIT_CREDENTIALS_PSW}", "UTF-8")
-  sh("echo password: ${GIT_PASSWORD}")
-
-  sh("git checkout -B ${params.branchName}")
-
-  // TODO need to hide git password from jenkins console
-  sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmUrl} ${params.branchName}")
+  withCredentials([usernamePassword(credentialsId: gitCredentialsId, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
+   	 	sh("echo username: ${GIT_USERNAME}")
+	    sh("echo password: ${GIT_PASSWORD}")
+  
+    	urlEncodedPassword = java.net.URLEncoder.encode("${env.GIT_CREDENTIALS_PSW}", "UTF-8")
+    	sh("echo ${urlEncodedPassword}")
+    
+		sh("git checkout -B ${params.branchName}")
+  		sh("git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${scmUrl} ${params.branchName}")
+	}
 }
