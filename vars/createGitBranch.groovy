162316303ba50e9cb40f8409290dbfad56b14d1e
Jon Lorusso <jon@swatt.exchange>
2018-08-20 18:52:17 +0000

    Add some debug output to create git branch groovy

--- vars/createGitBranch.groovy
+++ vars/createGitBranch.groovy
@@ -2,9 +2,14 @@ def call(params) {
   scmUrl = params.scmUrl ? params.scmUrl : scm.getUserRemoteConfigs()[0].getUrl().replaceAll('https://', '') 
   gitCredentialsId = params.gitCredentialsId ? params.gitCredentialsId : '80610dce-f3b7-428e-b69f-956eb087225d'
   
+  sh("echo ${scmUrl}")
+  sh("echo ${gitCredentialsId}")
   GIT_CREDENTIALS = credentials(gitCredentialsId)
+  sh("echo ${GIT_CREDENTIALS}")
   GIT_USERNAME = "${env.GIT_CREDENTIALS_USR}"
+  sh("echo ${GIT_USERNAME}")
   GIT_PASSWORD = java.net.URLEncoder.encode("${env.GIT_CREDENTIALS_PSW}", "UTF-8")
+  sh("echo ${GIT_PASSWORD}")
 
   sh("git checkout -B ${params.branchName}")
 
