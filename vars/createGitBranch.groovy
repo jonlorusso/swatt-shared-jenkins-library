21c5aaf0a6786e49c4eb07695c7dac4c41a4ed9e
Jon Lorusso <jon@swatt.exchange>
2018-08-20 19:23:56 +0000

    Pass git username and password into createGitBranch call

--- vars/createGitBranch.groovy
+++ vars/createGitBranch.groovy
@@ -2,14 +2,11 @@ def call(params) {
   scmUrl = params.scmUrl ? params.scmUrl : scm.getUserRemoteConfigs()[0].getUrl().replaceAll('https://', '') 
   gitCredentialsId = params.gitCredentialsId ? params.gitCredentialsId : '80610dce-f3b7-428e-b69f-956eb087225d'
   
-  withCredentials([usernamePassword(credentialsId: gitCredentialsId, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
-   	 	sh("echo username: ${GIT_USERNAME}")
-	    sh("echo password: ${GIT_PASSWORD}")
-  
-    	urlEncodedPassword = java.net.URLEncoder.encode("${env.GIT_CREDENTIALS_PSW}", "UTF-8")
-    	sh("echo ${urlEncodedPassword}")
+  sh("echo username: ${params.gitUsername}")
+  sh("echo password: ${params.gitPassword}")
     
-		sh("git checkout -B ${params.branchName}")
-  		sh("git push https://${GIT_USERNAME}:${urlEncodedPassword}@${scmUrl} ${params.branchName}")
-	}
+  sh("git checkout -B ${params.branchName}")
+  sh("set -x")
+  sh("git push https://${params.gitUsername}:${params.gitPassword}@${scmUrl} ${params.branchName}")
+  sh("set +x")	
 }
