f19d1edffb27701cf5cdf6678e939b2f2ee15b66
Jon Lorusso <jon@swatt.exchange>
2018-08-23 05:03:53 +0000

    Add params object to buildArgs ref

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(Map params) {
   registry = params.registry ? params.registry : "docker.dev.ruvpfs.swatt.exchange"
   dockerfile = params.dockerfile ? params.dockerfile : 'Dockerfile'
-  def argsString = buildArgs.collect{ k, v -> "--build-arg $k=$v" }.join(' ')
+  def argsString = params.buildArgs.collect{ k, v -> "--build-arg $k=$v" }.join(' ')
   sh "docker build --file ${dockerfile} ${argsString} --no-cache -t ${registry}/${params.imageName}:${params.tag} ."
 }
