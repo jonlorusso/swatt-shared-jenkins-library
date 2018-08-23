3f9795b97de3cdb686de864a416780951dcfe81b
Jon Lorusso <jon@swatt.exchange>
2018-08-23 04:41:09 +0000

    Remove extraneous debug statements

--- vars/createGitBranch.groovy
+++ vars/createGitBranch.groovy
@@ -1,12 +1,8 @@
 def call(params) {
   scmUrl = params.scmUrl ? params.scmUrl : scm.getUserRemoteConfigs()[0].getUrl().replaceAll('https://', '') 
-  gitCredentialsId = params.gitCredentialsId ? params.gitCredentialsId : '80610dce-f3b7-428e-b69f-956eb087225d'
-  
-  sh("echo username: ${params.gitUsername}")
-  sh("echo password: ${params.gitPassword}")
-    
-  sh("git checkout -B ${params.branchName}")
+
   sh("set -x")
+  sh("git checkout -B ${params.branchName}")
   sh("git push https://${params.gitUsername}:${params.gitPassword}@${scmUrl} ${params.branchName}")
   sh("set +x")	
 }
