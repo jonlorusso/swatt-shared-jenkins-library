d7646ba3fed9d11d9607fa59a2be536c3874f0fe
Jon Lorusso <jon@swatt.exchange>
2018-08-23 04:34:26 +0000

    Manually set params on dockerPush call within dockerTag function

--- vars/dockerTag.groovy
+++ vars/dockerTag.groovy
@@ -1,5 +1,5 @@
 def call(Map params) {
   registry = params.registry ? params.registry : "docker.dev.ruvpfs.swatt.exchange"
   sh "docker tag ${registry}/${params.imageName}:${params.sourceTag} ${registry}/${params.imageName}:${params.targetTag}"
-  dockerPush(params)
+  dockerPush imageName: params.imageName, tag: params.targetTag
 }
